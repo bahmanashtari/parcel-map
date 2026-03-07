9f03176d49fbca87e074263bdf641c3248b5bc14
Bahman.A <bahman778@gmail.com>
2026-03-06 22:17:15 -0800

    Remove unused TokenAuthentication and clean up requirements.txt

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -124,7 +124,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
